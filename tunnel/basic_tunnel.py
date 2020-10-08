3698cfe8f959c2d94b7e9117c4465f5c3ee9a7ad
barak <barakbs@gmail.com>
2020-10-08 20:37:36 +0300

    changed Tunnel to basic_tunnel, added doc and changes

---
+++ tunnel/basic_tunnel.py
@@ -0,0 +1,48 @@
+"""Class BasicTunnel."""
+
+import socket
+import traceback
+import threading
+
+from tunnel.ITunnel import ITunnel
+from config.tunnel.network import MAX_CHUNK_SIZE
+
+
+class BasicTunnel(ITunnel):
+    """A basic tunnel, implements ITunnel interface."""
+
+    def __init__(self, src_sock, dst_sock):
+        """Initialize BasicTunnel source and destination sockets.
+
+        Args:
+            src_sock (Socket): Source socket for the tunnel objcect.
+            dst_sock (Socket): Destination socket for the tunnel object.
+        """
+        self._src = src_sock
+        self._dst = dst_sock
+
+    def _tunnel_one_way(self, source, destination):
+        """Read chunks of data from source and sends them to destination.
+
+        Args:
+            source (Socket): Input socket to read the chunks from.
+            destination (Socket): Output socket to send the chunks to.
+        """
+        while True:
+            try:
+                data_read = source.recv(MAX_CHUNK_SIZE)
+                if len(data) > 0:
+                    destination.send(data_read)
+                else:
+                    break
+            except Exception as e:
+                print "Erorr on _tunnel_one_way: \r\n"
+                traceback.print_exc()
+                break
+
+    def tunnel(self):
+        """Tunnel between src_sock and dst_sock using two one way tunnels."""
+        threading.Thread(target=self._tunnel_one_way,
+                         args=(self._src, self._dst,)).start()
+        threading.Thread(target=self._tunnel_one_way,
+                         args=(self._dst, self._src,)).start()
