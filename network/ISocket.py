48ccf586cdf86e6eefeae1d4828a1bb6b0a4640d
YT <yuvaltv7@gmail.com>
2020-10-10 21:26:40 +0300

    add docs to  icmpsocket

--- network/ISocket.py
+++ network/ISocket.py
@@ -1,15 +1,47 @@
 class ISocket(object):
     def bind(self):
+        """Summary
+        bind to socket 
+        
+        Raises:
+            NotImplementedError: Description
+        """
         raise NotImplementedError
 
     def accept(self):
+        """Summary
+        Filter ICMP packets
+        Raises:
+            NotImplementedError: Description
+        """
         raise NotImplementedError
 
     def connect(self):
+        """Summary
+        
+        Raises:
+            NotImplementedError: Description
+        """
         raise NotImplementedError
 
     def send(self, data):
+        """Summary
+        Send data to the socket.
+        Args:
+            data (Byte): data to send
+        
+        Raises:
+            NotImplementedError: Description
+        """
         raise NotImplementedError
 
     def recv(self, amount):
+        """Summary
+        Receive data from socket
+        Args:
+            amount (Int): the maximum data to be received at once.
+        
+        Raises:
+            NotImplementedError: Description
+        """
         raise NotImplementedError
